9f02a7b261c3d9e5ca81836e8f9305cd17d85142
zayarmyooo19 <hanzohasashi880@gmail.com>
2024-01-04 18:37:47 +0630

    homework (Update)and (Delete) Refit

--- ZYMODotNetCorePractise/Program.cs
+++ ZYMODotNetCorePractise/Program.cs
@@ -1,19 +1,28 @@
-﻿using ZYMODotNetCorePractise.EFCoreExamples;
+﻿using AKLMPSTYZDotNetCore.ConsoleApp.RefitExamples;
+using ConsoleApp.DapperExamples;
+using ZYMODotNetCorePractise.EFCoreExamples;
 // AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 // adoDotNetExample.Run();
 
 // UserAdoDotNet userAdoDotNet = new UserAdoDotNet();
 // userAdoDotNet.Run();
 
-UserDapper userDapper = new UserDapper();
-userDapper.Run();
+//UserDapperService userDapper = new UserDapperService();
+//userDapper.Run();
 // UserDapper userDapper = new UserDapper();
 // userDapper.Run();
 
 // HttpClientExample httpClientExample = new HttpClientExample();
 // await httpClientExample.Run();
 
-RestClientExample restClientExample = new RestClientExample();
-await restClientExample.Run();
+
+//RestClientExample restClientExample = new RestClientExample();
+//await restClientExample.Run();
+
+Console.WriteLine("Please wait for api...");
+
+Console.ReadKey();
+RefitExample refitExample = new RefitExample();
+await refitExample.Run();
 
 Console.ReadKey();
---
+++ ZYMODotNetCorePractise/RefitExamples/IBlogApi.cs
@@ -0,0 +1,28 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZYMODotNetCorePractise.Models;
+
+namespace AKLMPSTYZDotNetCore.ConsoleApp.RefitExamples
+{
+    public interface IBlogApi
+    {
+        [Get("/api/blog")]
+        Task<List<BlogDataModel>> GetBlogs();
+
+        [Get("/api/blog/{id}")]
+        Task<BlogDataModel> GetBlog(int id);
+
+        [Post("/api/blog")]
+        Task<string> CreateBlog(BlogDataModel blog);
+
+        [Put("/api/blog/{id}")]
+        Task<string> UpdateBlog(int id, BlogDataModel blog);
+
+        [Delete("/api/blog/{id}")]
+        Task<string> DeleteBlog(int id);
+    }
+}
