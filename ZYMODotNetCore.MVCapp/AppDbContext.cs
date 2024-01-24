66a66a97c81ff2df60f0c1c1fb197cc0e1d0c8cc
zayarmyooo19 <hanzohasashi880@gmail.com>
2024-01-24 22:41:41 +0630

    C# MVC APP

---
+++ ZYMODotNetCore.MVCapp/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZYMODotNetCore.MVCapp.Models;
+
+namespace ZYMODotNetCore.MVCapp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogDataModel> Blogs { get; set; }
+
+    }
+}
---
+++ ZYMODotNetCore.MVCapp/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZYMODotNetCore.MVCapp.Models;
+
+namespace ZYMODotNetCore.MVCapp.Controllers
+{ // https://localhost:3000/blog/index
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BlogController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [ActionName("Index")]
+        public async Task<IActionResult> BlogIndex()
+        {
+            // "select * from tbl_blog with (nolock)"
+            List<BlogDataModel> lst = await _appDbContext.Blogs.AsNoTracking().ToListAsync();
+            //return View();
+            return View("BlogIndex", lst);
+        }
+    }
+}
